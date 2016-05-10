8e1b670d3a06d3f840a4ac6d3e08cd8cd983248e
StefanWerW <scaw.dev@gmail.com>
2016-05-10 23:22:43 +0200

    fix

--- create-ssh.js
+++ create-ssh.js
@@ -17,7 +17,7 @@ module.exports = function(){
     fs.writeFileSync(privateSsh, process.env.privategithubssh);
     fs.writeFileSync(publicSsh, process.env.publicgithubssh);
 
-    const addSshKey = spawn('eval "$(ssh-agent -s)" && ssh-add '+privateSsh);
+    var addSshKey = spawn('eval "$(ssh-agent -s)" && ssh-add '+privateSsh);
 
     addSshKey.stdout.on('data', (data) => {
       console.log(`stdout: ${data}`);
