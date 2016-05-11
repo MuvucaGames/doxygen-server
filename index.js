08bd60669ce17c3a48d04f4564cc2308d2a15244
StefanWerW <scaw.dev@gmail.com>
2016-05-11 14:26:36 +0200

    fixes

--- index.js
+++ index.js
@@ -9,10 +9,13 @@ var doxygen = require('./doxygen');
 var serveIndex = require('serve-index')
 const spawn = require('child_process').spawn;
 const exec = require('child_process').exec;
+var ncp = require('ncp').ncp;
+ncp.limit = 16;
 
-const siteGit = 'git@github.com:MuvucaGames/muvucagames.github.io.git'
 var game_dir = path.join(process.cwd(), 'MuvucaGame01/');
 var site_dir = path.join(process.cwd(), 'site/');
+var site_docs_dir = path.join(site_dir, 'taleoftwohearts/docs/');
+const pretext= "--------";
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -21,27 +24,35 @@ app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.post('/postpush', function(req, res) {
-    console.log("Recieved PUSH");
-    console.log("Deleting old folder");
+    console.log(pretext, "Recieved PUSH");
+    console.log(pretext, "Deleting old folder");
     rimraf(game_dir, function(){
-        console.log("Recreating folder");
+        console.log(pretext, "Recreating folder");
         if (!fs.existsSync(game_dir))
             fs.mkdirSync(game_dir);
-        console.log("Cloning repo");
+        console.log(pretext, "Cloning repo");
         clone("https://github.com/MuvucaGames/MuvucaGame01.git", game_dir, function(err){
             if(err){
-                console.error(err);
-                return;
+                return console.error(err);
             }
-            console.log("Clone game sucessful");
+            console.log(pretext, "Clone game sucessful");
             clone("https://github.com/MuvucaGames/muvucagames.github.io.git", site_dir, function(err){
-                console.log("Clone site sucessful");
+                console.log(pretext, "Clone site sucessful");
                 if(err){
-                    console.error(err);
-                    return;
+                    return console.error(err);
                 }
                 doxygen(function(){
-                    console.log("----------Doxy Created--------------------");
+                    console.log(pretext, "Doxy Created--------------------");
+                    rimraf(site_docs_dir, function(){
+                        if (!fs.existsSync(site_docs_dir))
+                            fs.mkdirSync(site_docs_dir);
+                        ncp(source, destination, function (err) {
+                            if (err) {
+                                return console.error(err);
+                            }
+                            console.log(pretext, 'copied docs to site dir');
+                        });
+                    });
                 });
             });
         });
