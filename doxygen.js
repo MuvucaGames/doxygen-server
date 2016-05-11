85be6d90ac97627eb59099bc7586f60eee61b1ab
StefanWerW <scaw.dev@gmail.com>
2016-05-11 15:19:08 +0200

    exec

--- doxygen.js
+++ doxygen.js
@@ -4,11 +4,23 @@ const doxygenBinPath = path.join(process.cwd(), 'doxygen/bin/doxygen');
 var mkdirp = require('mkdirp');
 
 const spawn = require('child_process').spawn;
-
+const exec = require('child_process').exec;
 
 module.exports = function(callback){
     console.log("..........doxing................");
     fs.chmodSync(doxygenBinPath, 0777);
+
+    const child = exec(doxygenBinPath,
+        (error, stdout, stderr) => {
+            console.log(`stdout: ${stdout}`);
+            console.log(`stderr: ${stderr}`);
+            if (error !== null) {
+                console.log(`exec error: ${error}`);
+            }
+            console.log("...........DOXY PROCESS EDNDED..........");
+            callback();
+        });
+    /*
     const doxy = spawn(doxygenBinPath);
 
     doxy.stdout.on('data', (data) => {
@@ -24,4 +36,5 @@ module.exports = function(callback){
       console.log("...............CALLING CALLBACK...............");
       callback();
     });
+    */
 }
